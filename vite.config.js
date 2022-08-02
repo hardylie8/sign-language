2c1f710896873c4a5b96f633f2584f50d6b2212f
hardyliyanto <hardyliyanto@gmail.com>
2022-08-02 19:51:21 +0700

    adjust composer

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+const path = require("path");
 
 export default defineConfig({
+    lib: {
+        entry: path.resolve(__dirname, "lib/main.js"),
+        name: "MyLib",
+        fileName: (format) => `my-lib.${format}.js`,
+    },
     plugins: [
         laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
+            input: ["resources/sass/app.scss", "resources/js/app.js"],
             refresh: true,
         }),
     ],
